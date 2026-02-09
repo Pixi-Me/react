6fabfc0883e73a55ff10d99b2796647575f12db2
Pixi-Me <ezioboii@gmail.com>
2026-02-09 14:06:38 +0530

    fixed numbering

--- src/components/section1/RightContent.jsx
+++ src/components/section1/RightContent.jsx
@@ -7,55 +7,53 @@ const RightContent = () => {
       img:'/src/assets/tom.jpg',
       txt:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque tenetur aspernatur inventore impedit? Odio, ea?",
       btn:"Lorem",
-      index:1
     },
     {
       img:"/src/assets/doodle.png",
       txt:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque tenetur aspernatur inventore impedit? Odio, ea?",
-      btn:"Lorem",
-      index:2
+      btn:"Lorem"
     },
     {
       img:"/src/assets/spiderMan.png",
       txt:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque tenetur aspernatur inventore impedit? Odio, ea?",
       btn:"Lorem",
-      index:3
+      
     }
     ,
     {
       img:"/src/assets/cat.jpg",
       txt:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque tenetur aspernatur inventore impedit? Odio, ea?",
       btn:"Lorem",
-      index:3
+      
     }
     ,
     {
       img:"/src/assets/fifth.png",
       txt:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque tenetur aspernatur inventore impedit? Odio, ea?",
       btn:"Lorem",
-      index:3
+      
     }
     ,
     {
       img:"/src/assets/sixth.png",
       txt:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque tenetur aspernatur inventore impedit? Odio, ea?",
       btn:"Lorem",
-      index:3
+      
     }
     ,
     {
       img:"/src/assets/seventh.jpg",
       txt:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque tenetur aspernatur inventore impedit? Odio, ea?",
       btn:"Lorem",
-      index:3
+       
     }
   ]
   return (
     <div className='w-[70%] h-auto flex gap-5 flex-nowrap overflow-x-scroll  overflow-y-visible [&::-webkit-scrollbar]:w-0'>
      {
-      arr.map((item)=>{
+      arr.map((item,index)=>{
         return(
-          <Card num={item.index} img={item.img} txt={item.txt} btn={item.btn}/>
+          <Card num={index+1} img={item.img} txt={item.txt} btn={item.btn}/>
         )
       })
      }
